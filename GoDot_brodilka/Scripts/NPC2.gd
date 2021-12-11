e9827637d9eee8f6c1df3ef8c3583c8a5f87c78b
ar26083-047132 <adilhanovadlet@gmail.com>
2021-12-11 10:37:50 +0600

    'fix_rebase_project'

---
+++ GoDot_brodilka/Scripts/NPC2.gd
@@ -0,0 +1,28 @@
+extends KinematicBody2D
+
+var speed = 200;
+var check = false;
+func _ready():
+	pass # Replace with function body.
+
+func _process(delta):
+	
+	var velocity = Vector2()
+	if speed >= 250:
+		check = true		
+	if speed <= 150:
+		check = false
+		
+		
+	if check == true:
+		velocity.y -= speed * 0.1
+		speed -= 1
+	if check == false:
+		velocity.y += speed * 0.1
+		speed += 1	
+		
+	
+	move_and_slide(velocity)
+	
+	position.x = clamp(position.x, 0, 970)
+	position.y = clamp(position.y, 0, 510)
